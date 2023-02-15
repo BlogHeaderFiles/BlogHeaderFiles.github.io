18347f96fe71bf06b2d7bb629122a5c98932212e
Carlos Buchart <cbuchart@gmail.com>
2023-02-15 12:23:59 +0100

    Revert "Minify JS"
    This reverts commit 1221a83078602bd44981dfa3eec4fbbe3092dd8c.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,42 @@
-window.onload=function(){SimpleJekyllSearch({searchInput:document.getElementById("searchbar"),resultsContainer:document.getElementById("search-results"),json:'{{ "/search.json" | relative_url }}',searchResultTemplate:'<a href="{url}" target="_blank">{title}</a>',noResultsText:""}),/Safari/.test(navigator.userAgent)&&!/Chrome/.test(navigator.userAgent)&&(document.body.firstElementChild.tabIndex=1);var e=document.querySelector(".posts-labelgroup"),t=document.getElementById("searchbar"),s=document.getElementById("posts-label"),l=document.querySelector(".search-results"),r=s.scrollWidth;s.style.width=r+"px",e.addEventListener("click",function(r){l.style.display=null,s.style.width="0",e.setAttribute("class","posts-labelgroup focus-within"),t.focus(),r.stopPropagation()},!1),e.addEventListener("mouseleave",function(){document.body.onclick=n});var n=function(t){l.style.display="none",e.setAttribute("class","posts-labelgroup"),s.style.width=r+"px",document.body.onclick=null}};
+---
+---
+
+window.onload = function () {
+    var sjs = SimpleJekyllSearch({
+        searchInput: document.getElementById('searchbar'),
+        resultsContainer: document.getElementById('search-results'),
+        json: '{{ "/search.json" | relative_url }}',
+        searchResultTemplate: '<a href="{url}" target="_blank">{title}</a>',
+        noResultsText: ''
+    });
+
+    /* hack ios safari unfocus */
+    if (/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent))
+        document.body.firstElementChild.tabIndex = 1;
+
+    var $labelGroup = document.querySelector(".posts-labelgroup");
+    var $searchbar = document.getElementById("searchbar");
+    var $postLabel = document.getElementById("posts-label");
+    var $searchResults = document.querySelector(".search-results");
+    var labelWidth = $postLabel.scrollWidth;
+    $postLabel.style.width = labelWidth + "px";
+
+    $labelGroup.addEventListener("click", function (e) {
+        $searchResults.style.display = null;
+        $postLabel.style.width = "0";
+        $labelGroup.setAttribute("class", "posts-labelgroup focus-within");
+        $searchbar.focus();
+        e.stopPropagation();
+    }, false);
+
+    $labelGroup.addEventListener("mouseleave", function () {
+        document.body.onclick = searchCollapse;
+    });
+
+    var searchCollapse = function (e) {
+        $searchResults.style.display = "none";
+        $labelGroup.setAttribute("class", "posts-labelgroup");
+        $postLabel.style.width = labelWidth + "px";
+        document.body.onclick = null;
+    };
+}
